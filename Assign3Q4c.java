5f41726cf2a7935400c9f0c1a8caa8e27fc3061f
Muhammad Hadeed Akram <160953486+mhadeedakram@users.noreply.github.com>
2024-02-22 21:45:10 +0000

    Update Assign3Q4c.java

--- Assign3Q4c.java
+++ Assign3Q4c.java
@@ -7,10 +7,10 @@ public class Assign3Q4c {
         int rows = 5;
 
        for(int i = 1; i <= rows; i++){
-        for(int j = 1; j <= i; j++){
-            System.out.print(" ");
+        for(int j = rows; j >= i; j--){
+            System.out.print("*");
         }
-        System.out.println("*");
+        System.out.println(" ");
        }
 
     }
